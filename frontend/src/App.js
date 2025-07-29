e6966197c984e3bb8d53777df2dcb3d850f8d2e2
SungHyun Park <sunghyun.park@take-it.co.kr>
2025-07-29 15:14:01 +0900

    왜 생김

--- frontend/src/App.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import "@/styles/App.css";
-import { BrowserRouter } from "react-router-dom";
-import AppRoutes from "./routes";
-function App() {
-    return (_jsx(BrowserRouter, { children: _jsx(AppRoutes, {}) }));
-}
-export default App;
--- frontend/src/pages/NotFound.js
+++
@@ -1,4 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-export default function NotFound() {
-    return _jsx("h1", { children: "404 - \uD398\uC774\uC9C0\uB97C \uCC3E\uC744 \uC218 \uC5C6\uC2B5\uB2C8\uB2E4" });
-}
